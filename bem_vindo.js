bcfafd378d4dc01843610cf756d0472d8dd876cc
Everton <everton.negromonte3@gmail.com>
2022-05-19 17:57:51 -0300

    Mentoria

---
+++ bem_vindo.js
@@ -0,0 +1,17 @@
+//todos rodam no CMD devico ao node.js
+console.log("Olá seja bem vindo");
+
+//Apresenta nome
+var nome ="Ricardo Alexandre";
+var sobrenome="Bontempo";
+
+console.log(nome+" "+sobrenome);
+
+// Criando as variáveis e inserindo valores
+let valor1=3020
+let valor2=1000
+
+function soma(x,y){
+    return x+y
+}
+console.log(soma(valor1,valor2))
